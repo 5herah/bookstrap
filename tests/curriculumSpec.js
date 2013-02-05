a9c8e3f4343dbff1a72db4ab212395befbe11fb0
Mark Rossetti <mark.a.rossetti@gmail.com>
2013-02-05 11:02:50 -0800

    Implemented two passing tests

--- tests/curriculumSpec.js
+++ tests/curriculumSpec.js
@@ -1,31 +1,23 @@
 // run tests at the command line with:
 // $ METEOR_MOCHA_TEST_DIR=tests mrt
 
-
 describe('curriculum', function () {
 
-  describe('first category', function () {
+  describe('curriculumlist', function () {
 
-    it('should', function () {
-      chai.assert.equal(10, 10);
+    it('getListings should set a session variable with a string (async)', function () {
+      Template.curriculumlist.getListings();
+      var text = Session.get('dirContents');
+      Meteor.setTimeout(function () {
+        chai.assert.isString(text);
+      }, 250);
     });
 
-  });
-  describe('second category', function () {
-
-    it('should', function () {
-      chai.assert.equal("waffles", "waffles");
-    });
-
-  });
-
-});
-
-describe('Array', function(){
-  describe('#indexOf()', function(){
-    it('should return -1 when the value is not present', function(){
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
+    it('displayListings should return a string (async)', function () {
+      var text = Template.curriculumlist.displayListings();
+      Meteor.setTimeout(function () {
+        chai.assert.isString(text);
+      }, 250);
     });
   });
 });
