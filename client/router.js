0f0c05a85930304188925d096cf351f3a1d69be7
Sherah <sherah@sherah.org>
2013-02-06 10:26:51 -0800

    Added the to-dos page to the router and main nav. All connected now.

--- client/router.js
+++ client/router.js
@@ -2,6 +2,7 @@ Meteor.pages({
   '/'                     : { to: 'home', as: 'root', nav: 'home'},
   '/curriculum'           : { to: 'curriculumlist', nav: 'curriculum' },
   '/curriculum/:filename' : { to: 'curriculumfile', nav: 'curriculum' },
+  '/sprintToDos'          : { to: 'sprintToDos', nav: 'todos' },
   '/tests'                : { to: 'tests' },
   '*'                     : { to: 'not_found' }
 });
