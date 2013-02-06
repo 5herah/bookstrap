beea34b1b21e980293646608c0b080e725d26764
tmschl <timothy.schiller@gmail.com>
2013-02-06 14:57:14 -0800

    Nav Changes

--- client/router.js
+++ client/router.js
@@ -2,11 +2,10 @@ Meteor.pages({
   '/'                     : { to: 'home', as: 'root' },
   '/curriculum'           : { to: 'curriculumlist' },
   '/curriculum/:filename' : { to: 'curriculumfile' },
-  '/directory'            : { to: 'profileInfo' },
   '/'                     : { to: 'home', as: 'root', nav: 'home'},
   '/curriculum'           : { to: 'curriculumlist', nav: 'curriculum' },
   '/curriculum/:filename' : { to: 'curriculumfile', nav: 'curriculum' },
-  '/directory'            : { to: 'profileInfo' },
+  '/directory'            : { to: 'profileInfo', nav: 'directory' },
   '/tests'                : { to: 'tests' },
   '*'                     : { to: 'not_found' }
 });
