7f3dd224f8d197d70a0dba42326453a6e414a000
Mark Rossetti <mark.a.rossetti@gmail.com>
2013-02-05 16:31:25 -0800

    Meteor methods refactored to use futures, preventing some async rendering issues

--- server/main.js
+++ server/main.js
@@ -1,10 +1,12 @@
 Meteor.methods({
   getFileSync: function (target) {
-    var text = fs.readFileSync(target, 'utf-8');
-    return text;
+    var future = new Future();
+    future.ret(fs.readFileSync(target, 'utf-8'));
+    return future.wait();
   },
   getDirContentsSync: function (directory) {
-    var list = fs.readdirSync(directory, 'utf-8');
-    return list;
+    var future = new Future();
+    future.ret(fs.readdirSync(directory, 'utf-8'));
+    return future.wait();
   }
 });
