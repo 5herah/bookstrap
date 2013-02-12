7dbdcdfbaeb86db4db80e2d69b2bb54814efa58b
Mark Rossetti <mark.a.rossetti@gmail.com>
2013-02-12 10:18:00 -0800

    Fixed bug with rendering readme markdown, and improved markdown display layout

--- client/curriculum/curriculumfile.js
+++ client/curriculum/curriculumfile.js
@@ -1,11 +1,15 @@
 Template.curriculumfile.getMarkdown = function () {
-  var path = window.location.pathname
+  var path = window.location.pathname;
   var filename = path.split('/').slice(-1);
   Meteor.call('getFileSync', 'public/readmes/' + filename, function (error, result) {
-    Session.set('fileText', result)
+    Session.set('fileText', result);
   });
 };
 
 Template.curriculumfile.displayMarkdown = function () {
   return Session.get('fileText');
+};
+
+Template.curriculumfile.readmeTitle = function () {
+  return window.location.pathname.split('/').slice(-1);
 };
--- client/curriculum/curriculumlist.js
+++ client/curriculum/curriculumlist.js
@@ -1,9 +1,20 @@
 Template.curriculumlist.getListings = function () {
   Meteor.call('getDirContentsSync', 'public/readmes', function (error, result) {
-    Session.set('dirContents', result)
+    Session.set('dirContents', result);
   });
 };
 
 Template.curriculumlist.displayListings = function () {
   return Session.get('dirContents');
 };
+
+Template.curriculumlist.events({
+  'click a': function (e) {
+    console.log(e.toElement.innerHTML);
+    var path = window.location.pathname;
+    var filename = path.split('/').slice(-1);
+    Meteor.call('getFileSync', 'public/readmes/' + filename, function (error, result) {
+     Session.set('fileText', result);
+    });
+  }
+});
