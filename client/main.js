b0d2bf1526e8cfc0e50948f3f81323261cb301f1
Mark Rossetti <mark.a.rossetti@gmail.com>
2013-02-17 11:32:31 -0800

    Fixed navbar classes bug caused by updating to new version of mini-pages

--- client/main.js
+++ client/main.js
@@ -1,9 +1,9 @@
 Handlebars.registerHelper('navClassFor', function (nav, options) {
-  return Session.equals('nav', nav) ? 'active' : ''
+  return Session.equals('mini-pages-router_nav', nav) ? 'active' : '';
 });
 
 Handlebars.registerHelper('isHome', function(nav){
-  return Session.equals('nav', 'home')
+  return Session.equals('mini-pages-router_nav', 'home');
 });
 
 Meteor.subscribe('userData');
